cca92b905e104b63f9f5d01e2e7c3f2a01a91b57
zhaicj0815 <1297093340@qq.com>
2019-11-19 17:49:09 +0800

    第三方

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id='detail'>
+   <detail-nav-bar></detail-nav-bar>
+    
+  </div>
+</template>
+
+<script>
+import DetailNavBar from './childComps/DetailNavBar'
+
+export default {
+  components: {
+    DetailNavBar
+  },
+  name: 'Detail',
+  props: {},
+  data() {
+    return {
+         iid: null
+    };
+  },
+  watch: {},
+  computed: {
+    
+  },
+  methods: {},
+  created() {
+    console.log('详情页初始化');
+    
+    this.iid = this.$route.params.iid
+  },
+  mounted() {}
+};
+</script>
+<style  scoped>
+</style>
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div >
+     <nav-bar >
+       <div slot='nav-left' class='back' @click='backClick'>
+         <img src="~assets/img/common/back.svg" alt="">
+       </div>
+      <div slot="nav-center" class='title'>
+        <div v-for='(item,index) in titles' class='title-item' :class="{active: index === currentIndex}" @click="titleClick(index)"> {{item}}</div>
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  components: {
+    NavBar
+  },
+  name: 'DetailNavBar',
+  props: {},
+  data() {
+    return {
+      titles: ['商品','参数','评论','推荐'],
+      currentIndex: 0,
+    };
+  },
+  watch: {},
+  computed: {
+    
+  },
+  methods: {
+    titleClick(index) {
+      this.currentIndex = index
+    },
+    backClick() {
+      this.$router.back()
+    }
+  },
+  created() {},
+  mounted() {}
+};
+</script>
+<style  scoped>
+  .title {
+    display: flex;
+    font-size: 13px;
+  }
+
+  .title-item {
+    flex: 1;
+  }
+
+  .active {
+    color: var(--color-high-text)
+  }
+
+  .back img {
+    width: 35%;
+    margin-top: 10px;
+  }
+</style>
