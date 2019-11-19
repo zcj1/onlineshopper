f7748202af3650770aa95807c3aaf99a67a3eb47
zcj1 <499907253@qq.com>
2019-11-20 00:42:23 +0800

    第三回合

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,40 @@
 <template>
   <div id='detail'>
    <detail-nav-bar></detail-nav-bar>
-    
+    <detail-swiper :top-images="topImages"></detail-swiper>
+    <detail-base-info :goods="goods" ></detail-base-info>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
+import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+
+import {getDetail,Goods,Shop} from 'network/detail'
 
 export default {
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo
   },
   name: 'Detail',
   props: {},
   data() {
     return {
-         iid: null
+      iid: null,
+      topImages: {
+        type: Array,
+        default(){
+          return []
+        }
+      },
+      goods: {},
+      shop: {},
     };
   },
   watch: {},
@@ -28,6 +46,24 @@ export default {
     console.log('详情页初始化');
     
     this.iid = this.$route.params.iid
+
+    getDetail(this.iid).then(res => {
+      const data = res.result
+      
+      this.topImages =data.itemInfo.topImages
+      console.log(this.topImages)
+      
+      this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+       console.log('============');
+        console.log(new Shop(data.shopInfo) );
+      this.shop = new Shop(data.shopInfo)
+      console.log('============');
+      console.log(this.shop );
+      
+      
+      
+      
+    })
   },
   mounted() {}
 };
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      <swiper :options="swiperOption" class='detail-swiper'>
+        <swiper-slide v-for="(slide, index) in topImages" :key="index">
+         
+              <img :src="slide" alt="" class='detail-swpier-img' >
+          
+        </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+     </swiper>
+    </div>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+
+export default {
+    name: 'DetailSwiper',
+    props: {
+      topImages: {
+        type: Array,
+        default: []
+      }
+    },
+    data() {
+        return {
+          swiperOption: {
+            pagination: {
+            el: '.swiper-pagination'
+            },
+            isLoad: false
+          },
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+      swiperSlide,
+      swiper
+    },
+};
+</script>
+
+<style scoped >
+  .detail-swiper {
+    height: 300px;
+  
+  }
+</style>
